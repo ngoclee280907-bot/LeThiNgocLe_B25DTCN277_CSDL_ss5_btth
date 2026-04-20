d21ee8330f43b6dab556cc4a2f827071864440b2
ngoclee_280907 <ngoclee280907@gmail.com>
2026-04-21 06:59:15 +0700

    Add files via upload

--- baitaptonghop.sql
+++ baitaptonghop.sql
@@ -1,19 +1,15 @@
-SELECT 
-    order_id,
-    user_id,
-    total_amount,
-    status,
-    note,
-    CASE 
-        WHEN total_amount > 4000000 THEN 'Nguy hiểm'
-        ELSE 'Bình thường'
-    END AS Alert_Level
+CREATE DATABASE adm;
+USE adm;
+
+CREATE TABLE Orders(
+total_amount INT NOT NULL,
+status VARCHAR(100) NOT NULL,
+note TEXT NOT NULL,
+user_id INT NOT NULL
+);
+SELECT total_amount,status,note,user_id
 FROM Orders
-WHERE total_amount BETWEEN 2000000 AND 5000000
-  AND status <> 'CANCELLED'
-  AND (
-        note LIKE '%gấp%'
-        OR user_id IS NULL
-      )
-ORDER BY total_amount DESC
+WHERE (total_amount BETWEEN 2000000 AND 5000000)
+AND status =! 'CANCELLED'
+AND (note LIKE '%gap%' OR user_id IS NULL
 LIMIT 20 OFFSET 40;
